fix nan gradient issue

constant_q_transform.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import librosa as lr
 import numpy as np
 import math
+import pdb
 
 def complex_multiply(a, b, complex_dim_a=None, complex_dim_b=None):
     # if a.shape != b.shape:
@@ -118,7 +119,7 @@ class CQT(nn.Module):
             # print(this_filter.shape)
             this_conv = nn.Conv1d(in_channels=1, out_channels=this_filter.shape[0], kernel_size=size, bias=False,
                                   stride=hop_length, padding=size // 2)
-            this_conv.weight = torch.nn.Parameter(this_filter.unsqueeze(1))
+            this_conv.weight = torch.nn.Parameter(this_filter.unsqueeze(1), requires_grad=False) # should be False
             self.conv_modules.append(this_conv)
 
     def forward(self, x):
@@ -137,3 +138,19 @@ class CQT(nn.Module):
         super().to(device)
         for conv in self.conv_modules:
             conv.to(device)
+
+import pydevd
+pydevd.settrace(suspend=False, trace_only_current_thread=True)
+
+def debug_hook(grad):
+    print("grad:", grad)
+    return grad
+
+def debug_backward_hook(module, grad_input, grad_output):
+    print("passed through backward hook")
+    diagnostic_hook(module, grad_input, grad_output)
+    pass
+
+def diagnostic_hook(module, grad_input, grad_output):
+    module += 1
+    print("module:", module, "- grad in:", grad_input, "- grad out:", grad_output)

parallel_wavenet_trainer.py
@@ -11,10 +11,12 @@ class ParallelWavenetTrainer:
         self.model = model
         self.dataset = dataset
         self.cqt_module = cqt_module
+        self.loss_func = nn.MSELoss()
         self.epsilon = 1e-10
 
         self.cqt_time = 0
         self.model_time = 0
+        self.loading_time = 0
 
     def train(self,
               batch_size=32,
@@ -26,18 +28,21 @@ class ParallelWavenetTrainer:
         optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
         dataloader = torch.utils.data.DataLoader(self.dataset,
                                                  batch_size=batch_size,
-                                                 shuffle=True,
+                                                 shuffle=False, #True,
                                                  num_workers=num_workers,
                                                  pin_memory=False)
 
         dev = next(self.model.parameters()).device
 
         step = continue_training_at_step
+        tic = time.time()
         for current_epoch in range(epochs):
             print("epoch", current_epoch)
             for batch in iter(dataloader):
-
+                toc = time.time()
+                self.loading_time = toc - tic
                 example_signal = torch.cat([batch[0], batch[1]], dim=2).to(dev)
+                example_signal.requires_grad = False
                 tic = time.time()
                 example_cqt = torch.log(abs(self.cqt_module(example_signal))**2 + self.epsilon)
                 toc = time.time()
@@ -52,9 +57,14 @@ class ParallelWavenetTrainer:
 
                 # create output cqt
                 output_signal = torch.cat([batch[0].to(dev), output], dim=2).to(dev)
-                output_cqt = torch.log(abs(self.cqt_module(output_signal))**2 + self.epsilon)
+                output_cqt = self.cqt_module(output_signal)
+                output_cqt = abs(output_cqt + self.epsilon)**2
+                output_cqt = torch.log(output_cqt + self.epsilon)
 
-                loss = torch.mean((output_cqt - example_cqt)**2)
+                loss = self.loss_func(output_cqt, example_cqt)
+
+                if torch.isnan(loss).item() > 0:
+                    print("error, loss is nan.")
 
                 self.model.zero_grad()
                 loss.backward()
@@ -63,8 +73,10 @@ class ParallelWavenetTrainer:
                 step += 1
 
                 if step % 1 == 0:
-                    print("step", step, "- loss:", loss.item(), "- cqt:", self.cqt_time, "s - model:", self.model_time)
+                    print("step", step, "- loss:", loss.item(), "- cqt:", self.cqt_time, "s - model:", self.model_time,
+                          "s - loading_time:", self.loading_time, "s")
 
+                tic = time.time()
 #
 #
 # class WavenetTrainer:

wavenet_dataset.py
@@ -100,7 +100,8 @@ class ParallelWavenetDataset(torch.utils.data.Dataset):
             data, _ = lr.load(str(file), sr=self.sampling_rate, mono=self.mono, dtype=np.float32)
             new_name = 'file_' + str(i) + ".wav"
             new_file = self.dataset_path / new_name
-            lr.output.write_wav(str(new_file), data, sr=self.sampling_rate)
+            soundfile.write(str(new_file), data, samplerate=self.sampling_rate, subtype='PCM_16')
+            #lr.output.write_wav(str(new_file), data, sr=self.sampling_rate)
             print("processed " + str(file))
 
     def calculate_length(self):